baa42edba2d97c9d5575b7b252a6562b32d26a7b
cst7785 <cestevens95@gmail.com>
2022-06-16 11:51:43 -0400

    Created start of file structure for the app.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = '4001';
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    let context = {};
+    res.render('index.ejs', context)
+});
+
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
