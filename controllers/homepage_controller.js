497adcac8cb28ff915972f127a53bfec63c98903
cst7785 <cestevens95@gmail.com>
2022-06-21 16:07:50 -0400

    Added a delete route.

--- controllers/homepage_controller.js
+++ controllers/homepage_controller.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next)=>{
 })
 router.get('/characters/:id', async (req, res, next)=>{
     try {
-        const character = await db.Character.findById(req.params.id);
+        const character = await db.Character.findById(req.params.id).populate('stats');
         context = {character}
         res.render('show.ejs', context)
     } catch (error) {
@@ -37,7 +37,16 @@ router.get('/edit/:id', async (req, res, next)=>{
 router.put('/characters/:id', async (req, res, next) => {
     try {
         let updatedCharacterInformation = await db.Character.findByIdAndUpdate(req.params.id, req.body);
-        // characterInformation = await db.Character.findByIdAndUpdate(req.params.id, {stats: stats})
+        console.log(updatedCharacterInformation.stats._id)
+        let updatedStats = {
+            character: req.params.id,
+            strength: req.body.strength,
+            dexterity: req.body.dexterity,
+            constitution: req.body.constitution,
+            intelligence: req.body.intelligence,
+            wisdom: req.body.wisdom,
+            charisma: req.body.charisma};
+        updatedStats = await db.Stats.findByIdAndUpdate(updatedCharacterInformation.stats, updatedStats)
         res.redirect(`/characters/${updatedCharacterInformation._id}`);
     } catch (error) {
         console.log(error);
@@ -71,4 +80,17 @@ router.post('/', async (req, res, next) => {
         return next();
     }
 })
+router.delete('/characters/:id', async (req,res, next)=>{
+    try {
+        const deletedCharacter = await db.Character.findByIdAndDelete(req.params.id);
+        const deletedStats = await db.Stats.findByIdAndDelete(deletedCharacter.stats)
+        return res.redirect('/')
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
+})
+
+
 module.exports = router;
