72fd71cf7fe23d94566d9411476bc973c91feeb4
cst7785 <cestevens95@gmail.com>
2022-06-21 11:08:32 -0400

    Added some temporary routes for future implementation.

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    homepage: require('./homepage_controller'),
+    manual: require('./manual_controller'),
+    forums: require('./forums_controller'),
+    signin: require('./signin_controller')
+}
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const app = express();
+const controllers = require('./controllers');
 const PORT = '4001';
 
 // require('./config/db.connection')
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+app.use('/', controllers.homepage);
+app.use('/manual', controllers.manual);
+app.use('/forums', controllers.forums);
+app.use('/signin', controllers.signin);
 
-
-app.get('/', (req, res) => {
-    let context = {};
-    res.render('index.ejs', context)
-});
+// app.get('/', (req, res) => {
+//     let context = {};
+//     res.render('index.ejs', context)
+// });
 
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
